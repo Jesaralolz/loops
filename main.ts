a5d52a9149bde04167939ce2914b99e29dd763ff
Jesaralolz <101340154+Jesaralolz@users.noreply.github.com>
2022-04-12 08:48:01 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,35 @@
 input.onButtonPressed(Button.A, function () {
     Sprite.turn(Direction.Left, 180)
     for (let index = 0; index < 4; index++) {
-        for (let index = 0; index < 4; index++) {
-            Sprite.move(1)
-        }
+        Sprite.move(1)
+        basic.pause(200)
+    }
+    for (let index = 0; index < 4; index++) {
+        Sprite.turn(Direction.Right, 180)
+        Sprite.change(LedSpriteProperty.X, 1)
+        Sprite.change(LedSpriteProperty.Y, 1)
+        basic.pause(200)
+    }
+    Sprite.turn(Direction.Right, 90)
+    for (let index = 0; index < 4; index++) {
+        Sprite.move(1)
+        basic.pause(200)
+    }
+    Sprite.turn(Direction.Left, 90)
+    for (let index = 0; index < 4; index++) {
+        Sprite.move(1)
+        Sprite.change(LedSpriteProperty.Y, 1)
+        basic.pause(200)
+    }
+    Sprite.turn(Direction.Right, 90)
+    for (let index = 0; index < 4; index++) {
+        Sprite.move(1)
+        basic.pause(200)
+    }
+    Sprite.turn(Direction.Right, 90)
+    for (let index = 0; index < 4; index++) {
+        Sprite.move(1)
+        basic.pause(200)
     }
 })
 input.onButtonPressed(Button.B, function () {
